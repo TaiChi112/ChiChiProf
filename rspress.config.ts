69269b937004fc13453650281081910394ea02be
TaiChi <anothai.0978452316@gmail.com>
2025-09-13 22:10:32 +0700

    feat: enhance rspress configuration with additional social links and routing options

--- rspress.config.ts
+++ rspress.config.ts
@@ -1,22 +1,43 @@
 import * as path from 'node:path';
 import { defineConfig } from 'rspress/config';
+// import image_leetcode from './docs/images/leetcode.png';
 
 export default defineConfig({
   root: path.join(__dirname, 'docs'),
-  outDir:'public',
+  outDir: 'public',
   title: 'My Site',
   icon: '/rspress-icon.png',
   logo: {
     light: '/rspress-light-logo.png',
     dark: '/rspress-dark-logo.png',
   },
+
   themeConfig: {
+    // nav:[
+    //   { text: 'Guide', link: '/guide/guide' },
+    //   { text: 'Blog', link: '/blog/blog' },
+    //   { text: 'Projects', link: '/project/project' },
+    // ],
     socialLinks: [
       {
         icon: 'github',
         mode: 'link',
         content: 'https://github.com/TaiChi112',
       },
+      {
+        icon: 'discord',
+        mode: 'link',
+        content: 'https://discord.com/invite/your-invite-code',
+      },
+      {
+        icon: 'linkedin',
+        mode: 'link',
+        content: 'https://www.linkedin.com/in/anothai-vichapaiboon-a88790260/',
+      }
     ],
   },
+  route: {
+    exclude: ['components/**/*'],
+    include: ['other-dir/**/*']
+  }
 });
